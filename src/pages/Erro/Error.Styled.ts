7c2319d1d513021ec7774cb6d2ea7881a69495b5
Renan Cesar <cesar.renan@hotmail.com>
2024-03-17 10:41:16 -0300

    feat: responsividade error page

--- src/pages/Erro/Error.Styled.ts
+++ src/pages/Erro/Error.Styled.ts
@@ -35,4 +35,29 @@ export const Container = styled.div`
     color: var(--font-color-secondary);
     font-weight: bold;
   }
+
+  @media (max-width: 500px) {
+    min-height: calc(100vh - 100px);
+    padding: 100px var(--padding-lateral-mobile);
+    
+    .container-geral {
+      flex-direction: column;
+      margin-top: 60px;
+
+      > img {
+        height: 250px;
+        width: 180px;
+      }
+
+      h1 {
+        width: 100%;
+        align-items: center;
+        font-size: 28px;
+      }
+    }
+
+    p {
+      text-align: center;
+    }
+  }
 `;
